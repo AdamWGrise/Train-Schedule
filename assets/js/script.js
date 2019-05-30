6120678aa2bdafc8b7f418cdc400e9faf2fa880a
Adam Grise <adamwgrise@gmail.com>
2019-05-29 23:18:41 -0500

    Init commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAVA3exkrV7DMW6DIgH0K7juHgtZNXh2rc",
+    authDomain: "train-schedule-1c021.firebaseapp.com",
+    databaseURL: "https://train-schedule-1c021.firebaseio.com",
+    projectId: "train-schedule-1c021",
+    storageBucket: "train-schedule-1c021.appspot.com",
+    messagingSenderId: "156242379899",
+    appId: "1:156242379899:web:c88594616452160a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
